402b5290bf7b4264ecb282a7d6895d63eb5b3889
Akki Mahesh <Maheshakki799@gmail.com>
2026-09-08 18:43:25 +0530

    Enhance login page navigation and input handling
    Updated the login page navigation to wait for network idle and increased timeout. Changed input selectors for username and password fields.

--- playwright/login.js
+++ playwright/login.js
@@ -15,16 +15,29 @@ const { chromium } = require("playwright");
 
     const page = await context.newPage();
 
-    await page.goto("http://devopswithmahesh.shop/login");
+    await page.goto("http://devopswithmahesh.shop/login", {
+        waitUntil: "networkidle",
+        timeout: 60000
+    });
 
-    await page.fill('input[type="number"]', process.env.APP_USERNAME);
-    await page.fill('input[type="password"]', process.env.APP_PASSWORD);
+    console.log("Current URL:", page.url());
+    console.log("Title:", await page.title());
+
+    await page.fill(
+        'input[placeholder="Enter mobile or username"]',
+        process.env.APP_USERNAME
+    );
+
+    await page.fill(
+        'input[placeholder="Enter password"]',
+        process.env.APP_PASSWORD
+    );
 
     await page.click('button[type="submit"]');
 
     await page.waitForLoadState("networkidle");
 
-    console.log("Current URL:", page.url());
+    console.log("Login URL:", page.url());
 
     await browser.close();
 
