e5f19abbf0ff51895781c40dcc670a916e466215
Akki Mahesh <Maheshakki799@gmail.com>
2026-09-08 18:32:46 +0530

    Change email input to number input in login script

--- playwright/login.js
+++ playwright/login.js
@@ -17,7 +17,7 @@ const { chromium } = require("playwright");
 
     await page.goto("http://devopswithmahesh.shop/login");
 
-    await page.fill('input[type="email"]', process.env.APP_USERNAME);
+    await page.fill('input[type="number"]', process.env.APP_USERNAME);
     await page.fill('input[type="password"]', process.env.APP_PASSWORD);
 
     await page.click('button[type="submit"]');
